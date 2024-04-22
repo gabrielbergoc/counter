e13b07f72e904af41566ad79921c3a9600731ec6
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2024-04-22 01:02:53 -0300

    show nothing when still loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,33 @@
 "use client";
 
-import { redirect } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Button from "./components/button/button";
 import Display from "./components/display/display";
 import Header from "./components/header/header";
-import { isAuthenticated } from "./lib/auth";
+import Navbar from "./components/navbar/navbar";
+import { isAuthenticated, removeToken } from "./lib/auth";
 import { getUserCounter, setUserCounter } from "./lib/counter";
 import "./page.scss";
 
 export default function Home() {
   const router = useRouter();
+  const [show, setShow] = useState(false);
   const [counter, setCounter] = useState(0);
+
+  const setShowTrue = () => setShow(true);
 
   useEffect(() => {
     if (!isAuthenticated()) {
       redirect("/login");
     }
-    
-    getUserCounter().then(setCounter);
+
+    getUserCounter()
+      .then(setCounter)
+      .then(setShowTrue);
   }, []);
   useEffect(() => {
-    setUserCounter(counter)
+    setUserCounter(counter);
   }, [counter]);
 
   function addToCounter(n: number) {
@@ -40,18 +46,29 @@ export default function Home() {
   const increment = addToCounter(+1);
 
   return (
-    <main className="main">
-      <Header>Counter</Header>
-      <div className="grid">
-        <div className="row">
-          <Button className="counter-buttons" onclick={decrement}>-</Button>
-          <Display className="counter">{counter}</Display>
-          <Button className="counter-buttons" onclick={increment}>+</Button>
-        </div>
-        <div className="row">
-          <Button onclick={resetCounter}>Reset</Button>
-        </div>
-      </div>
-    </main>
+    <>
+      {show && (
+        <main className="main">
+          <Navbar>
+            <Button onclick={doLogout}>Logout</Button>
+          </Navbar>
+          <Header>Counter</Header>
+          <div className="grid">
+            <div className="row">
+              <Button className="counter-buttons" onclick={decrement}>
+                -
+              </Button>
+              <Display className="counter">{counter}</Display>
+              <Button className="counter-buttons" onclick={increment}>
+                +
+              </Button>
+            </div>
+            <div className="row">
+              <Button onclick={resetCounter}>Reset</Button>
+            </div>
+          </div>
+        </main>
+      )}
+    </>
   );
 }
