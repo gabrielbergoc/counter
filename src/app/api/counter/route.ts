4f55026a59eb8a6112b6a3182aa8737c3a67cf7b
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2024-04-21 23:47:43 -0300

    api/counter: POST method to update user counter

--- src/app/api/counter/route.ts
+++ src/app/api/counter/route.ts
@@ -32,3 +32,35 @@ export async function GET(req: NextRequest) {
     return NextResponse.redirect("/login");
   }
 }
+
+export async function POST(req: NextRequest) {
+  if (!hasAuthorizationHeaders(req)) {
+    return NextResponse.redirect("/login");
+  }
+
+  const token = getTokenFromHeaders(req);
+  if (!token) {
+    return NextResponse.redirect("/login");
+  }
+
+  const { counter } = await req.json();
+
+  if (counter === null || counter === undefined || typeof counter !== "number") {
+    return NextResponse.json({ message: "Something went wrong" }, { status: 500 });
+  }
+
+  try {
+    const payload = await authorize(token);
+
+    const { email } = payload;
+    await prisma.user.update({
+      where: { email },
+      data: { counter },
+    });
+
+    return NextResponse.json({ counter });
+
+  } catch (err) {
+    return NextResponse.redirect("/login");
+  }
+}
