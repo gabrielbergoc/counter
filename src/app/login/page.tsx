cb8c566e208238770cb5a7810944c1dad00b195e
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2024-04-21 21:24:56 -0300

    login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,39 @@
+"use client";
+
+import { useState } from "react";
+import Input from "../components/input/input";
+import Button from "../components/button/button";
+
 export default function Page() {
-    return <p>login works!</p>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function doLogin() {
+    const res = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+    });
+    const { token } = await res.json();
+
+    localStorage.setItem("token", token);
+  }
+
+  return (
+    <main className="main">
+      <Input
+        id="login-email"
+        label="E-mail"
+        value={email}
+        onchange={setEmail}
+      />
+      <Input
+        id="login-password"
+        type="password"
+        label="Password"
+        value={password}
+        onchange={setPassword}
+      />
+      <Button onclick={doLogin}>Login</Button>
+    </main>
+  );
 }
