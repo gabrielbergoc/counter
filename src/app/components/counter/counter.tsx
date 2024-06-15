8af4cb96c87429c32db8c82b56233ddba9d23de2
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2024-06-15 13:04:41 -0300

    replace minus and plus signs with svg icons to improve visual and readability

--- src/app/components/counter/counter.tsx
+++ src/app/components/counter/counter.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import plusIcon from "../../../../public/plus.svg";
+import minusIcon from "../../../../public/minus.svg";
+import { setUserCounter } from "@/app/lib/server/counter";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import Button from "../button/button";
+import Dialog from "../dialog/dialog";
 import Display from "../display/display";
 import Header from "../header/header";
 import "./counter.scss";
-import { setUserCounter } from "@/app/lib/server/counter";
-import Dialog from "../dialog/dialog";
 
 export default function Counter({ initialValue }: { initialValue?: number }) {
   const [counter, setCounter] = useState(initialValue ?? 0);
@@ -51,11 +54,11 @@ export default function Counter({ initialValue }: { initialValue?: number }) {
       <div className="grid">
         <div className="row">
           <Button className="counter-buttons" onclick={decrement}>
-            -
+            <Image src={minusIcon} alt="Decrement counter" width={15} height={15} />
           </Button>
           <Display className="counter">{counter}</Display>
           <Button className="counter-buttons" onclick={increment}>
-            +
+            <Image src={plusIcon} alt="Increment counter" width={15} height={15} />
           </Button>
         </div>
 
